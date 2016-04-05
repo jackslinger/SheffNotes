582035bab78858d0e24ba7eba2e990ab8fb0df52
Jack Slinger <jackslinger10@gmail.com>
2016-04-05 12:22:32 +0100

    Added some mocked courses to the courses page.

--- spec/features/user_visits_courses_page_spec.rb
+++ spec/features/user_visits_courses_page_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
-feature 'User visits Courses page' do
-  scenario 'successfully' do
+feature 'Courses Page' do
+  scenario 'Vists the page succesfully' do
     visit root_path
     expect(page).to have_css 'h1', text: 'Courses'
+  end
+
+  scenario 'Views courses that they are a part of' do
+    #Login is not implemented yet so the system is mocked to represent one user
+
+    visit root_path
+    expect(page).to have_link('COM4513 Natural Langauge Processing')
+    expect(page).to have_link('COM4519 Cloud Computing')
   end
 end
