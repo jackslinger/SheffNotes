0a9e7d98277f11be15ecb0a69332fc2a133125c0
Jack Slinger <jackslinger10@gmail.com>
2016-04-13 14:43:33 +0000

    Restriced users to only seeing their own courses.

---
+++ app/models/ability.rb
@@ -0,0 +1,8 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :read, Course, :participations => { :user_id => user.id }
+  end
+end
