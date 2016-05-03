5506c318357455be6c8dd2a7e1a3fcfcf2b2f522
Jack Slinger <jackslinger10@gmail.com>
2016-05-03 15:32:49 +0000

    Moved viewing notes to it's own page.

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,7 @@ class Note < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :document, styles: { large: "1024x1024>", thumb: "128x128>" }, :default_url => "/images/missing/notes/:style/missing.png", :preserve_files => false
-  validates_attachment :document, content_type: {content_type: ['image/jpeg', 'image/png']}
+  validates_attachment :document, content_type: {content_type: ['image/jpeg', 'image/png', 'application/pdf']}
   # validates :user, :course, presence: true
   validates :document, :title, :course, presence: true
 end
